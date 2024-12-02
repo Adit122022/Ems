6438d0eb03ab35f3c3d52ce40b22d252326967ec
Adit122022 <sharmaji742595@gmail.com>
2024-12-02 10:45:42 +0530

    login completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './Components/Auth/Login'
 import EmployeDashboard from './Components/Dashboard/EmployeDashboard'
 import AdminDashboard from './Components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 const App = () => {
-  useEffect(() => {
-    // setLocalStorage()
-  getLocalStorage();
-},)
+  const [user, setUser] = useState(null)
+  const handleLogin =(email,password) => {
+if(email =='admin@me.com'  && password =='123'){
+setUser('admin')
+console.log(user)
+  }else if(email == 'usern@me.com' && password == '123'){
+    setUser('employee')
+    console.log(user)
+  }
+  else{
+    alert('Invalid Credentials! ')
+  }
+  }
+
+//   useEffect(() => {
+//     // setLocalStorage()
+//   getLocalStorage();
+// },)
   
   return (
    <>
-   <Login/>
-   {/* <EmployeDashboard/> */}
-   {/* <AdminDashboard/> */}
+{!user ?    <Login handleLogin ={handleLogin}/> : ''}
+{user == 'admin' ?   <AdminDashboard/>  :  <EmployeDashboard/>}
+  
+   
 
    </>
   )
