9246801307ce5b83531ab9e049d56516b09f5b9c
Aditya Sharma <adity202212@gmail.com>
2025-03-31 04:12:07 +0530

    Update EmployeDashboard.jsx

--- src/Components/Dashboard/EmployeDashboard.jsx
+++ src/Components/Dashboard/EmployeDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../others/Header'
 import TaskListNumber from '../others/TaskListNumber'
-import Tasklist from '../TaskList/tasklist'
+import Tasklist from '../TaskList/Tasklist'
 
 
 const EmployeDashboard = ({ data,changeUser}) => {
